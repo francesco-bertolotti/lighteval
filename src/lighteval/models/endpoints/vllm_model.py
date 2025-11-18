2044cc895145dc56915a61c16d1052e7825cee8a
Francesco Bertolotti <francesco.bertolotti@igenius.ai>
2025-11-18 17:17:59 +0100

    cache key does not account for the endpoint and headers

--- src/lighteval/models/endpoints/vllm_model.py
+++ src/lighteval/models/endpoints/vllm_model.py
@@ -1,15 +1,15 @@
 import concurrent
 import diskcache
 import lighteval
+import requests
+import pydantic
 import hashlib
 import logging
-import requests
 import typing
 import rich
 import json
 
 logger = logging.getLogger(__name__)
-
 
 class VLLMClientConfig(lighteval.models.abstract_model.ModelConfig):
     model_name: str 
@@ -33,71 +33,99 @@ class VLLMClient(lighteval.models.abstract_model.LightevalModel):
 
     def __init__(self, config: VLLMClientConfig = None) -> None:
         self.config = config
+        self.model_name = config.model_name
+        self.base_url = config.base_url
+        self.api_key = config.api_key
+        self.max_parallel = config.max_parallel
+        self.timeout = config.timeout
+        self.max_tokens = config.max_tokens
+        self.extra_body = config.extra_body
+        self.temperature = config.temperature
+        self.top_p = config.top_p
+        self.top_k = config.top_k
+        self.min_p = config.min_p
+        self.frequency_penalty = config.frequency_penalty
+        self.presence_penalty = config.presence_penalty
+        self.seed = config.seed
+        self.cache_dir = config.cache_dir
         self._cache = lighteval.utils.cache_management.SampleCache(config.model_copy(update={"cache_dir": "/tmp"}))
 
-    #@lighteval.utils.cache_management.cached(lighteval.tasks.requests.SamplingMethod.GENERATIVE)
     def greedy_until(self, docs: list[lighteval.tasks.requests.Doc]) -> list[lighteval.models.model_output.ModelResponse]:
 
-        def call(doc: lighteval.tasks.requests.Doc) -> lighteval.models.model_output.ModelResponse:
+        def call(doc: lighteval.tasks.requests.Doc, cache:diskcache.Cache) -> lighteval.models.model_output.ModelResponse:
             """ Make a single request to the model endpoint and return the response. """
 
             # prepare request
-            request_params = {
-                "url" : f"{self.config.base_url}/chat/completions",
-                "headers": {
-                    "Authorization": f"Bearer {self.config.api_key}",
-                    "Content-Type": "application/json",
-                },
-                "json" : {
-                    "model": self.config.model_name,
-                    "messages": (
-                        messages := (
-                            [{"role":"system", "content": doc.instruction}] if doc.instruction else []) + 
-                            [{"role": "user", "content": doc.query}]
-                        ),
-                    "n": doc.num_samples,
-                    "max_tokens": self.config.max_tokens,
-                    "temperature": self.config.temperature,
-                    "top_p": self.config.top_p,
-                    "min_p": self.config.min_p,
-                    "frequency_penalty": self.config.frequency_penalty,
-                    "presence_penalty": self.config.presence_penalty,
-                    "seed": self.config.seed,
-                    **self.config.extra_body,
-                },
-                "timeout" : self.config.timeout,
+            json_params = { 
+                "model": self.model_name,
+                "messages": (
+                    messages := (
+                        [{"role":"system", "content": doc.instruction}] if doc.instruction else []) + 
+                        [{"role": "user", "content": doc.query}]
+                    ),
+                "n": doc.num_samples,
+                "max_tokens": self.max_tokens,
+                "temperature": self.temperature,
+                "top_p": self.top_p,
+                "min_p": self.min_p,
+                "frequency_penalty": self.frequency_penalty,
+                "presence_penalty": self.presence_penalty,
+                "seed": self.seed,
+                **self.extra_body,
             }
 
             # check cache
-            with diskcache.Cache(self.config.cache_dir or "/tmp/vllm_cache") as cache:
-                key = hashlib.sha256(json.dumps(request_params, sort_keys=True).encode()).hexdigest()
-                if key not in cache: cache[key] = {"response" : requests.post(**request_params).json(), "request" : request_params}
+            key = hashlib.sha256(json.dumps(json_params, sort_keys=True).encode()).hexdigest()
+            if key in cache:
                 response = cache[key]["response"]
+            else:
+                response = requests.post(
+                    url=f"{self.base_url}/chat/completions",
+                    headers={
+                        "Authorization": f"Bearer {self.api_key}",
+                        "Content-Type": "application/json",
+                    },
+                    json=json_params,
+                    timeout=self.timeout
+                ).json()
+                cache[key] = {"response" : response, "request" : json_params}
+            
 
-            # check for errors
-            if response.get("code",200) != 200:
-                raise RuntimeError(f"VLLM API error: {response}")
-
-            # parse response
-            response = lighteval.models.model_output.ModelResponse(
-                text=[choice["message"]["content"] for choice in response["choices"]],
-                reasonings=[choice["message"]["reasoning_content"] for choice in response["choices"]],
-                input=messages,
-            )
-
-            return response
+            match response.get("error", {}).get("code", 200):
+                case 200:
+                    return lighteval.models.model_output.ModelResponse(
+                        text=[choice["message"]["content"] for choice in response["choices"]],
+                        reasonings=[choice["message"]["reasoning_content"] for choice in response["choices"]],
+                        input=messages,
+                    )
+                case 400:
+                    logger.warning(f"VLLM API returned 400 Bad Request: {response}, returning empty responses.")
+                    return lighteval.models.model_output.ModelResponse(
+                        text=["" for _ in range(doc.num_samples)],
+                        reasonings=["" for _ in range(doc.num_samples)],
+                        input=messages,
+                    )
+                case _:
+                    raise RuntimeError(f"VLLM API error: {response}")
 
         responses: typing.List[typing.Optional[lighteval.models.model_output.ModelResponse]] = [None] * len(docs)
     
-        with rich.progress.Progress("[progress.description]{task.description}", rich.progress.BarColumn(), "[progress.percentage]{task.percentage:>3.0f}%", "•", rich.progress.TimeElapsedColumn(), "•", rich.progress.TimeRemainingColumn()) as pbar:
-            pbar.add_task(description="Sending requests...", total=len(docs))
-            with concurrent.futures.ThreadPoolExecutor(max_workers=self.config.max_parallel) as executor:
-                futures = {executor.submit(call, doc): idx for idx, doc in enumerate(docs)}
+        with rich.progress.Progress(
+            "[progress.description]{task.description}", 
+            rich.progress.BarColumn(), 
+            "[progress.completed]{task.completed}/{task.total}", "•",
+            rich.progress.TimeElapsedColumn(), "•", 
+            rich.progress.TimeRemainingColumn()
+        ) as pbar:
+            with diskcache.Cache(self.cache_dir or "/tmp/vllm_cache") as cache:
+                task = pbar.add_task(description="Sending requests...", total=len(docs))
+                with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_parallel) as executor:
+                    futures = {executor.submit(call, doc, cache): idx for idx, doc in enumerate(docs)}
 
-                for future in concurrent.futures.as_completed(futures):
-                    idx = futures[future]
-                    responses[idx] = future.result()
-                    pbar.update(0, advance=1)
+                    for future in concurrent.futures.as_completed(futures):
+                        idx = futures[future]
+                        responses[idx] = future.result()
+                        pbar.update(task, advance=1)
 
         return responses
 
@@ -116,10 +144,8 @@ class VLLMClient(lighteval.models.abstract_model.LightevalModel):
     def max_length(self) -> int:
         raise NotImplementedError("VLLM client does not have a max length.")
 
-    #@lighteval.utils.cache_management.cached(lighteval.tasks.requests.SamplingMethod.LOGPROBS)
     def loglikelihood(self, docs: list[lighteval.tasks.requests.Doc]) -> list[lighteval.models.model_output.ModelResponse]:
         raise NotImplementedError("VLLM client does not support loglikelihood computation.")
 
-    #@lighteval.utils.cache_management.cached(lighteval.tasks.requests.SamplingMethod.PERPLEXITY)
     def loglikelihood_rolling(self, docs: list[lighteval.tasks.requests.Doc]) -> list[lighteval.models.model_output.ModelResponse]:
         raise NotImplementedError("VLLM client does not support rolling loglikelihood computation.")
